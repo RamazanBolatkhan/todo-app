efa7e79ecdaad74f576eab81dc0cfb8781c821ca
RamazanB <ramazanbolathan04@gmail.com>
2022-07-10 21:54:28 +0600

    Added git, and added migrations. First commit

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/RamazanBolatkhan/todo-app"
+	"github.com/RamazanBolatkhan/todo-app/pkg/handler"
+	"github.com/RamazanBolatkhan/todo-app/pkg/repository"
+	"github.com/RamazanBolatkhan/todo-app/pkg/service"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while runnig http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
