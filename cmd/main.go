6e4fb25a387c76f5075a49b4f73aa5ec3c216b22
RamazanB <ramazanbolathan04@gmail.com>
2022-07-11 16:23:33 +0600

    lesson 5, connecting DB environment variables, library sqlx

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/RamazanBolatkhan/todo-app"
 	"github.com/RamazanBolatkhan/todo-app/pkg/handler"
 	"github.com/RamazanBolatkhan/todo-app/pkg/repository"
 	"github.com/RamazanBolatkhan/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -14,7 +17,24 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
